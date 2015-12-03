cd29f0ffac80915b39dab6bfa10c0e3fd84ffadb
Sameer Khan <sameerkhan2k@gmail.com>
2015-12-03 13:50:39 +0530

    Cleanup
    Fix the orientation change issue and code cleanup.

--- app/src/main/java/com/sameer/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/sameer/android/myappportfolio/MainActivity.java
@@ -1,13 +1,10 @@
 package com.sameer.android.myappportfolio;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,19 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_spotify: showToast("Spotify Streamer");
-                break;
-            case R.id.button_scores: showToast("Scores");
-                break;
-            case R.id.button_library: showToast("Library");
-                break;
-            case R.id.button_build_it: showToast("Build It Bigger");
-                break;
-            case R.id.button_xyz_reader: showToast("XYZ Reader");
-                break;
-            case R.id.button_capstone: showToast("Capstone");
-                break;
-        }
+        String message = ((Button) view).getText().toString();
+        showToast(message);
     }
 }
